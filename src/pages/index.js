84009192ba523138a7e50c931a0dd23dd13bf64c
Sunscar-Sony <166742286+Sunscar-Sony@users.noreply.github.com>
2024-09-04 01:42:45 +0530

    Meta Data Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>ImagineAI</title>
+    </Head>
       <h1>Hello ImagineAI</h1>
     </>
   );
