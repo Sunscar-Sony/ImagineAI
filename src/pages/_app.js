49190a40683d6c20c6790e565bf65360f4b2f8e1
Sunscar-Sony <166742286+Sunscar-Sony@users.noreply.github.com>
2024-09-04 14:34:18 +0530

    temp updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="Generate Image of your Imagination with ImagineAI" />
+        <meta name="author" content="Sanskar Soni" />
+        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
