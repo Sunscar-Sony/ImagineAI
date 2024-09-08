d70530c3f2c79f835095d02183771ddfd066fd9d
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-09-08 17:29:28 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,15 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
 
+const Navbar = () => (
+  <nav className={styles.navbar}>
+    <div className={styles.logo}>MyAIChatbot</div>
+    <ul className={styles.navLinks}>
+      <li><Link href="#">Home</Link></li>
+      <li><Link href="#">Features</Link></li>
+      <li><Link href="#">Contact</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
