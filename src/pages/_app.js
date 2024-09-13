f380bb32d6ff4582d79ad977d71fa733f901d927
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-09-13 09:21:45 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,1 @@
-import Head from "next/head";
-import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="description" content="Interact with ImagineAI to sove your problems" />
-        <meta name="author" content="Sanskar Soni" />
-        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
