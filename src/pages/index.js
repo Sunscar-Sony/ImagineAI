bb0beea6d8f9707b670a5806ebdc4948d926b40b
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-09-15 09:55:08 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import ChatbotSection from '../components/ChatbotSection';
+import Footer from '../components/Footer';
 
+const Home = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+    <ChatbotSection />
+    <Footer />
+  </>
+);
+
+export default Home;
